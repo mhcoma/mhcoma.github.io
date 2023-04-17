48792cf44a76eabdf7920a0a8440f83d54b3c5d4
Coma <bmh4080@naver.com>
2023-04-17 22:08:35 +0900

    .

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function get_parameter_by_name(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
